8c5517c56c61e5ec292b718855b8e80b86d93556
priyesh <priyesh.bhawsar@pepsico.com>
2025-06-27 19:16:14 +0530

    added start script

---
+++ docker-compose/start-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+docker compose \
+  -f immich/immich.yml \
+  -f jellyfin.yml \
+  -f karakeep/karakeep.yml \
+  -f linkwarden/linkwarden.yml \
+  -f monitoring.yml \
+  -f network.yml \
+  -f obsidian/obsidian.yml \
+  -f pdf.yml \
+  -f servarr.yml \
+  -f soulseek.yml \
+  -f tandoor/tandoor.yml \
+  -f tools.yml \
+  up -d
