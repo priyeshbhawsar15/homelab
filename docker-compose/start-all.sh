cd9c32f6735dcee729a86fc4567fdef26c6d4334
priyesh <priyesh.bhawsar@pepsico.com>
2025-06-28 17:33:39 +0530

    same folder

--- docker-compose/start-all.sh
+++ docker-compose/start-all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 docker compose \
-  -f immich/immich.yml \
+  -f immich.yml \
   -f jellyfin.yml \
-  -f karakeep/karakeep.yml \
+  -f karakeep.yml \
   -f monitoring.yml \
   -f obsidian/obsidian.yml \
   -f pdf.yml \
   -f servarr.yml \
   -f soulseek.yml \
-  -f tandoor/tandoor.yml \
+  -f tandoor.yml \
   -f tools.yml \
   up -d
   # -f linkwarden/linkwarden.yml \
